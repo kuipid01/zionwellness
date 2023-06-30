7344fc7ff4b667e118c487df30abcbae260e3f80
unknown <kuipid01@gmail.com>
2023-06-30 15:02:16 +0200

    added framer motion and navmobility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,57 @@
-import React from 'react'
-import { FaCopyright } from 'react-icons/fa'
+import React from "react";
+import { AiFillPhone, AiOutlineArrowRight, AiOutlineMail } from "react-icons/ai";
+import { FaCopyright } from "react-icons/fa";
 const Footer = () => {
-    return (
-        <div className='w-full flex justify-center items-center bg-black h-fit py-5'>
-            <p className='text-gray-500 w-fit font-bold text-base md:text-lg items-center flex mx-auto text-center'>
-                Copyright &copy; Devkuipid |   Inspired <br className='flex md:hidden' />  by Major Wellness Center - All Rights Reserved
+  return (
+    <div className="w-full flex flex-col gap-2  justify-center items-center bg-red-500 opacity-70 h-fit py-5">
+      <div className="md:w-[80%] w-full px-3 py-20 text-white justify-between gap-9 md:gap-0 flex md:flex-row flex-col mx-auto h-fit ">
+        <div className="md:w-[30%] mb-9 w-full">
+            <h1 className="text-4xl text-center mb-3">Logo</h1>
+          {/* <img src="https://scicarers.com.au/wp-content/uploads/2021/05/sci-care-1024x265.jpg" alt="" /> */}
+          <p className="md:text-left leading-[30px] text-center font-medium md:font-semibold text-[15px]">
+            Zion Wellness Services: Empowering disabilities, connecting
+            communities, fostering well-being.
+          </p>
+        </div>
+        <div>
+            <h1 className="mb-5">Quick Links</h1>
+            <hr className="text-[5px] mb-4" />
+            <ul className="flex flex-col gap-2">
+                <li>Our Team</li>
+                <li> Testimonials</li>
+                <li>Contact Us</li>
+                <li>About Zion Wellness</li>
+            </ul>
+        </div>
+        <div>
+            <h1 className="mb-5">Official information</h1>
+            <hr className="text-[5px] mb-4" />
+            <p className="flex  mb-8 items-center gap-3">
+                 <AiOutlineMail/>
+                 <span> abc@example.com </span>
+                
             </p>
         </div>
-    )
-}
+        <div>
+            <h1 className="mb-5">Call Now</h1>
+            <hr className="text-[5px] mb-4" />
+            <p  className="flex  mb-8 items-center gap-3">
+                 <AiFillPhone/>
+               <span> 12345678 </span> 
+            </p>
+            <div className="w-[300px] relative flex justify-center py-1 items-center bg-white h-[50px]">
+                <input type="text" className="w-full h-full text-black outline-none pl-3 bg-transparent" placeholder="Enter your email to join" />
+                <button className="absolute flex font-[700] px-5  bg-red-700 justify-center items-center gap-2 right-0 h-full "> <span>Send</span>  <AiOutlineArrowRight/> </button>
+            </div>
+        </div>
+      </div>
+      <hr className="w-full h-[2px] text-white" />
+      <p className="text-white w-fit font-bold text-base md:text-base items-center flex mx-auto text-center">
+        Copyright &copy; Devkuipid | Inspired <br className="flex md:hidden" />{" "}
+        by Major Wellness Center - All Rights Reserved
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,10 +4,10 @@ const Slider = () => {
     const [slideValue, setSlideValue] = useState(0)
     const images  = [
         'https://plus.unsplash.com/premium_photo-1663134272749-95163ecfff20?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80',
-          'https://images.unsplash.com/photo-1599818497948-eea9a2698ddd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
-'https://images.unsplash.com/photo-1478061653917-455ba7f4a541?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1150&q=80',
+          'https://images.unsplash.com/photo-1599817086997-131b19b42f67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
+'https://images.unsplash.com/photo-1469573054742-64da3f2527fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1026&q=80',
 
-'https://images.unsplash.com/photo-1581090122319-8fab9528eaaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80']
+'https://images.unsplash.com/photo-1526662092594-e98c1e356d6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80']
     let sliderStyle = {
         transform: `translateX(${slideValue}vw)`,
         transition: 'transform 0.4s ease-in-out',
@@ -17,6 +17,7 @@ const Slider = () => {
 setSlideValue(-index*100)
     }
     // useEffect(() => {
+
     //     setInterval(() => {
     //         if (slideValue===0) {
                 
@@ -46,7 +47,7 @@ setSlideValue(-index*100)
             }
         </div>
 
-<div className='flex h-full w-full opacity-20 left-0 bg-black bottom-0 absolute' >
+<div className='flex h-full w-full opacity-10 left-0 bg-black bottom-0 absolute' >
 
 </div>
 <div className='flex gap-6 left-1/2 -translate-x-1/2 bottom-14 absolute'>
